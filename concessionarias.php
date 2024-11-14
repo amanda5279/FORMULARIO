1e834af17a5929f41e3b14db0da8769f0714bff7
amanda5279 <amandaluize5279@gmail.com>
2024-11-14 14:05:41 -0300

    Update concessionarias.php

---
+++ concessionarias.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário Concessionaria</title>
+</head>
+<body>
+
+<h2>Formulário Concessionaria</h2>
+
+<form action="processa_alocacao.php" method="POST">
+    <label>Nome Concessionaria:</label>
+    <input type="text" name="concessionaria" required><br><br>
+
+    <button type="submit">Enviar</button>
+</form>
+
+</body>
+</html>
